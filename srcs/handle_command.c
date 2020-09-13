afeaa20ab0b1bd4d6271ddb0a9fcf330beba4092
YoukjinLee <ukjinlee66@gmail.com>
2020-09-13 20:42:12 +0900

    fin

--- srcs/handle_command.c
+++ srcs/handle_command.c
@@ -6,10 +6,19 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 16:23:44 by youlee            #+#    #+#             */
-/*   Updated: 2020/09/12 16:26:14 by youlee           ###   ########.fr       */
+/*   Updated: 2020/09/13 20:41:34 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	handle_command(t_list *command_lines)
+{
+	t_list		*current;
 
+	while (command_lines)
+	{
+		current = command_lines;
+		if (current->data == "")
+	}
+}
