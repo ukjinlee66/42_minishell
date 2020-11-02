41a23c3f00303dadf8757b5d5a4855a48cac163c
YoukjinLee <ukjinlee66@gmail.com>
2020-11-02 21:48:05 +0900

    echo add sender

--- srcs/command_func/command_echo.c
+++ srcs/command_func/command_echo.c
@@ -6,7 +6,7 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 16:15:59 by youlee            #+#    #+#             */
-/*   Updated: 2020/11/02 21:41:14 by youlee           ###   ########.fr       */
+/*   Updated: 2020/11/02 21:47:49 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,25 @@ void		command_echo(char **argv, int *receiver, int *sender)
 	char *res;
 	char *temp;
 	int len;
+	int fd;
 
 	len = 0;
+	if (sender[0] == -1)
+		fd = 1;
+	else
+		fd = 125; //임의의 값.
 	cnt = 1;
-	//in 저장 out ->
-/*	int cnt;
-	int read_ret;
-
-	cnt = 0;
-	while (read_ret = read())
-*/
 	if (argv[1] == NULL) //단일 에코
-		write(1, "NULL\n", 5);
+		write(fd, "NULL\n", 5);
 	else
 	{
 		temp = ft_strjoin("", "");
 		while (argv[cnt])
 		{
-			write(1, "\n", 1);
+			write(fd, "\n", 1);
 			res = ft_strjoin(temp, argv[cnt]);
-			ft_putstr_fd(argv[cnt], 1);
-			write(1, "\n", 1);
+			ft_putstr_fd(argv[cnt], fd);
+			write(fd, "\n", 1);
 			free(temp);
 			temp = res;
 			cnt++;	
@@ -47,7 +45,7 @@ void		command_echo(char **argv, int *receiver, int *sender)
 		while (argv[cnt])
 			free(argv[cnt++]);
 		free(argv);
-		write(1, temp, ft_strlen(temp));
+		write(fd, temp, ft_strlen(temp));
 		//pipe add code
 		free(temp);
 	}
