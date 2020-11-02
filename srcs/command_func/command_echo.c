f2cc02761d2d91113c436f475dc3a50a7c53176d
YoukjinLee <ukjinlee66@gmail.com>
2020-11-02 21:02:41 +0900

    yet

--- srcs/command_func/command_echo.c
+++ srcs/command_func/command_echo.c
@@ -6,7 +6,7 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 16:15:59 by youlee            #+#    #+#             */
-/*   Updated: 2020/11/02 20:20:38 by youlee           ###   ########.fr       */
+/*   Updated: 2020/11/02 21:02:31 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 void		command_echo(char **argv, int *receiver, int *sender)
 {
+	int argv1_len;
+
+	argv1_len = 0;
 	//in 저장 out ->
 /*	int cnt;
 	int read_ret;
@@ -22,4 +25,9 @@ void		command_echo(char **argv, int *receiver, int *sender)
 	while (read_ret = read())
 */
 	printf("argv[0] : %s\nargv[1] : %s\n",argv[0],argv[1]);
+	if (argv[1] == NULL) //단일 에코
+		write(1, "\n", 1);
+	while (argv[1][argv1_len])
+		argv1_len++;
+	if (argv[1][0] == '"' && )
 }
