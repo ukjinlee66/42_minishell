a4ad2fad975607001a60b37a754e1fa3a5f851cd
YoukjinLee <ukjinlee66@gmail.com>
2021-01-20 02:40:51 +0900

    relative, absolute fin

--- srcs/command_func/command_absolute_run.c
+++ srcs/command_func/command_absolute_run.c
@@ -6,7 +6,7 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/19 05:45:28 by youlee            #+#    #+#             */
-/*   Updated: 2021/01/19 05:47:27 by youlee           ###   ########.fr       */
+/*   Updated: 2021/01/20 02:39:32 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ void	command_absolute_run(char **argv, int *receiver, int *sender)
 	char	*command;
 	char	**envp;
 
-	path = ft_strdup(argv[0] + 1);
+	path = ft_strdup(argv[0]);
 	envp = make_envp();
 	command = make_com(argv[0]);
 	argv[0] = command;
 	idx = 0;
 	if (execve(path, argv, envp) == -1)
-		printf("execve error\n");
+		printf("execve absolute error\n");
 	else
 		printf("execve run!\n");	
 }
@@ -49,16 +49,15 @@ char	**make_envp(void)
 	int		idx;
 
 	idx = 0;
-	while (envl[idx][0])
+	while (envl[idx][0] != '\0')
 		idx++;
 	temp = (char**)malloc(sizeof(char*) * (idx + 1));
 	idx = 0;
-	while (envl[idx][0])
+	while (envl[idx][0] != '\0')
 	{
 		temp[idx] = ft_strdup(envl[idx]);
-		printf("temp[idx] : %s\n", envl[idx]);
 		idx++;
 	}
-	temp[idx] = 0;
+	temp[idx] = NULL;
 	return (temp);
 }
--- srcs/command_func/command_relative_run.c
+++ srcs/command_func/command_relative_run.c
@@ -6,12 +6,11 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/19 19:58:55 by youlee            #+#    #+#             */
-/*   Updated: 2021/01/19 05:48:18 by youlee           ###   ########.fr       */
+/*   Updated: 2021/01/20 02:36:35 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
 
-extern char **environ;
 
 void	command_relative_run(char **argv, int *receiver, int *sender)
 {
@@ -20,13 +19,13 @@ void	command_relative_run(char **argv, int *receiver, int *sender)
 	char	*command;
 	char	**envp;
 
-	path = ft_strdup(argv[0]);
+	path = ft_strdup(argv[0] + 2);
 	envp = make_envp();
 	command = make_com(argv[0]);
 	argv[0] = command;
 	idx = 0;
 	if (execve(path, argv, envp) == -1)
-		printf("execve error\n");
+		printf("execve relative error\n");
 	else
 		printf("execve run!\n");
 }
