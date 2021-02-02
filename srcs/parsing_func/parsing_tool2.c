bcb0a13a632489a71c49847b6626dd38d53d3c61
YoukjinLee <ukjinlee66@gmail.com>
2021-02-02 20:42:33 +0900

    f

--- srcs/parsing_func/parsing_tool2.c
+++ srcs/parsing_func/parsing_tool2.c
@@ -6,11 +6,27 @@
 /*   By: sseo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 18:22:37 by sseo              #+#    #+#             */
-/*   Updated: 2021/02/02 19:42:45 by sseo             ###   ########.fr       */
+/*   Updated: 2021/02/02 20:42:23 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int			get_arg_type(const char *arg)
+{
+	if (ft_strcmp(arg, ">") == 0)
+		return (1);
+	else if (ft_strcmp(arg, ">>") == 0)
+		return (2);
+	else if (ft_strcmp(arg, "<") == 0)
+		return (3);
+	else if (ft_strcmp(arg, ";") == 0)
+		return (4);
+	else if (ft_strcmp(arg, "|") == 0)
+		return (5);
+	else
+		return (0);
+}
 
 int			parse_double_quote_loop(const char *line, char **p_data, \
 		size_t *p_start, size_t *p_len)
