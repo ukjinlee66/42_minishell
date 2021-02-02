62674cf38552b0cbee569e59282c4d591bc83cfd
YoukjinLee <ukjinlee66@gmail.com>
2021-02-02 20:05:26 +0900

    handle norm

--- srcs/command_func/handle_command.c
+++ srcs/command_func/handle_command.c
@@ -6,12 +6,22 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 16:23:44 by youlee            #+#    #+#             */
-/*   Updated: 2021/02/02 15:13:31 by sseo             ###   ########.fr       */
+/*   Updated: 2021/02/02 20:03:59 by youlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	else_exec(t_list **p_first_elem, int *receiver, int *sender)
+{
+	execute_command(*p_first_elem, 0, receiver, sender);
+	wait(0);
+}
+
+static void	null_ret(void)
+{
+	return ;
+}
 void		handle_command(t_list **p_first_elem, int *receiver,
 		int *sender)
 {
@@ -38,10 +48,6 @@ void		handle_command(t_list **p_first_elem, int *receiver,
 	else if (arg_type == 4)
 		arg_semicolon(p_first_elem, current, receiver, sender);
 	else if (arg_type == 5)
-		arg_pipe(p_first_elem, before, receiver ,sender);
-	else
-	{
-		execute_command(*p_first_elem, 0, receiver, sender);
-		wait(0);
-	}
+		arg_pipe(p_first_elem, before, receiver, sender);
+	!arg_type ? else_exec(p_first_elem, receiver, sender) : null_ret();
 }
