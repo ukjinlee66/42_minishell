ec268e0f6b534331c8782e133fcb158dfee0b93b
sseo <tjdalsemc@gmail.com>
2021-02-02 17:21:29 +0900

    norm

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: youlee <youlee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 15:15:53 by youlee            #+#    #+#             */
-/*   Updated: 2021/02/02 13:24:50 by youlee           ###   ########.fr       */
+/*   Updated: 2021/02/02 17:08:09 by sseo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,6 @@ void		main_process(void)
 	int				receiver[1000];
 	int				sender[1000];
 
-	getcwd(g_cur_path, PATH_SIZE);
-	write(1, g_cur_path, ft_strlen(g_cur_path));
-	write(1, "$ ", 2);
 	if ((gnl_ret = get_interactive_line(&line)) < 0)
 		exit(2);
 	if (gnl_ret == 0)
@@ -50,6 +47,9 @@ int			main(int argc, char **argv, char **envp)
 		{
 			signal(SIGINT, handle_sig);
 			signal(SIGQUIT, handle_sig2);
+			getcwd(g_cur_path, PATH_SIZE);
+			write(1, g_cur_path, ft_strlen(g_cur_path));
+			write(1, "$ ", 2);
 			main_process();
 		}
 	}
